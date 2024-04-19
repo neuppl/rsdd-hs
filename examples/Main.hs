0ded6c62a63d0787c8ed043b8b6abe5829e07e5d
Sam Stites <stites@users.noreply.github.com>
2024-04-19 16:32:16 -0400

    bump flake.lock

--- examples/Main.hs
+++ examples/Main.hs
@@ -12,13 +12,13 @@ main = do
   let t = ptrTrue mgr
   ite mgr a b t >>= putStrLn . show
   ite mgr t t t >>= putStrLn . show
-  let wmc = newWmc
-  setWeight wmc la $ Weight { hi = (1/3), lo = (2/3) }
-  setWeight wmc lb $ Weight { hi = (1/4), lo = (3/4) }
-  print $ varWeight wmc la
-  print $ varWeight wmc lb
+  let w = newWmc
+  setWeight w la $ Weight { hi = (1/3), lo = (2/3) }
+  setWeight w lb $ Weight { hi = (1/4), lo = (3/4) }
+  print $ varWeight w la
+  print $ varWeight w lb
   a_or_b <- bddOr mgr a b
   printBdd a_or_b >>= putStrLn
-  let x = bddWmc a_or_b wmc
+  let x = bddWmc a_or_b w
   print ("hello", x)
   pure ()
